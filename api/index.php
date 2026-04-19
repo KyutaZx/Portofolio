c3797fdab4909fe09b2628264d16086f9eaf9884
Muhammad Nur Hidayatulloh <mdayatulloh27@gmail.com>
2026-04-19 23:30:26 +0700

    Test PHP runtime with Hello World

--- api/index.php
+++ api/index.php
@@ -1,15 +1,19 @@
 <?php
-// Override path storage untuk Vercel (karena Vercel read-only)
-$storagePath = '/tmp/storage/framework';
-$folders = ['views', 'sessions', 'cache'];
-foreach ($folders as $folder) {
-    if (!is_dir($storagePath . '/' . $folder)) {
-        mkdir($storagePath . '/' . $folder, 0755, true);
-    }
-}
+echo "PHP Runtime is working! <br>";
+echo "PHP Version: " . phpversion() . "<br>";
+echo "Current Time: " . date('Y-m-d H:i:s') . "<br>";
 
-putenv("VIEW_COMPILED_PATH=$storagePath/views");
-putenv("SESSION_DRIVER=cookie"); // Hindari menulis session ke file
-
-// Forward request to public/index.php milik Laravel
-require __DIR__ . '/../public/index.php';
+// // Override path storage untuk Vercel (karena Vercel read-only)
+// $storagePath = '/tmp/storage/framework';
+// $folders = ['views', 'sessions', 'cache'];
+// foreach ($folders as $folder) {
+//     if (!is_dir($storagePath . '/' . $folder)) {
+//         mkdir($storagePath . '/' . $folder, 0755, true);
+//     }
+// }
+// 
+// putenv("VIEW_COMPILED_PATH=$storagePath/views");
+// putenv("SESSION_DRIVER=cookie"); 
+// 
+// // Forward request to public/index.php milik Laravel
+// require __DIR__ . '/../public/index.php';
