142d0ec0164eba507996b02976cf268300aa1504
Muhammad Nur Hidayatulloh <mdayatulloh27@gmail.com>
2026-04-19 23:45:46 +0700

    Enable Laravel bootstrap after successful reachability test

--- api/index.php
+++ api/index.php
@@ -1,5 +1,4 @@
 <?php
-die("DEBUG: API INDEX IS RUNNING - Kalo muncul ini berarti Vercel sudah benar panggil file ini.");
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
